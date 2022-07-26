30d20b2e56614dd39c3c6fd1966def7b4546a3a3
Nithya Thayananthan <nithyathaya@gmail.com>
2022-07-26 14:38:49 -0400

    auth sample

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,12 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NewsComponent } from './components/news/news.component';
 import { PostsComponent } from './components/posts/posts.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home/:id', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  { path: 'about', component: AboutComponent, canActivate: [AuthGuard] },
   { path: 'contact', component: ContactComponent },
   { path: 'news', component: NewsComponent },
   { path: 'grocery', component: GroceryComponent },
